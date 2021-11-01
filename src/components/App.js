6b9c90faa68da093973195c30e0910b70aa571b5
Rafael Vargas <rafael.vargas@outlook.com>
2021-10-31 23:40:52 -0400

    add avatar import

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import Header from "./Header";
 import Pirate from "./Pirate";
 import piratesFile from "../data/sample-pirates-array";
+import avatar from "../assets/img/avatar.png";
 
 const pirateCalls = [
   "Aaarg, belay that!",
@@ -14,12 +15,13 @@ function randomize() {
 }
 
 function App() {
+  const [pirates, setPirates] = React.useState(piratesFile);
   return (
     <div>
       <Header title={randomize()} />
       <div className="pirate">
         {piratesFile.map((pirate) => (
-          <Pirate tagline={randomize()} pirate={pirate} />
+          <Pirate key={pirate.name} tagline={randomize()} pirate={pirate} />
         ))}
       </div>
     </div>
