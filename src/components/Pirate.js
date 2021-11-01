f22b02ebdef1d30b616652a3a09f25d2a1f569f4
Rafael Vargas <rafael.vargas@outlook.com>
2021-10-31 23:48:23 -0400

    destructure props in Pirate.js

--- src/components/Pirate.js
+++ src/components/Pirate.js
@@ -2,12 +2,25 @@ import React from "react";
 import "../assets/css/Pirate.css";
 import avatar from "../assets/img/avatar.png";
 
-
 function Pirate(props) {
+  const { name, year, weapon, vessel, description } = props.pirate;
+  const { tagline } = props;
+
   return (
     <section>
-      <h3>{props.pirate.name}</h3>
-      <p>Favorite saying: {props.tagline}</p>
+      <summary>
+        <img src={avatar} alt="pirate" />
+        <h3>{name}</h3>
+        <ul>
+          <li>Died: {year}</li>
+          <li>Weapon: {weapon}</li>
+          <li>Vessel: {vessel}</li>
+        </ul>
+      </summary>
+      <article>
+        <h2>{tagline}</h2>
+        <p>{description}</p>
+      </article>
     </section>
   );
 }
